e7e5f7917bb060ed73757a8c6036e2e0c7928e66
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2018-12-16 14:06:51 +0100

    Use configs

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,28 @@
+import config = require("config");
 import express = require("express");
 import serialport = require("serialport");
 
 const api = express();
-const port: number = 3000;
+const port: number = config.has("api.port") ? config.get("api.port") : 3000;
+const apiInfo = {
+    name: "weatherduino_api",
+    version: "0.1",
+};
+
+api.get("/api", (req, res) => {
+    res.json(apiInfo);
+});
+
+api.get("/weather", (req, res) => {
+    res.json(["x"]);
+});
+
+api.get("/weather/indoor", (req, res) => {
+    res.json(["y"]);
+});
+
+api.get("/weather/outdoor", (req, res) => {
+    res.json(["z"]);
+});
 
 api.listen(port);
