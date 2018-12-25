e1f5ecdac6ab2c7b77b13525f11647d90e0fb1f7
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2018-12-25 21:36:27 +0100

    Do not publish via CI

--- app/src/background.ts
+++ app/src/background.ts
@@ -7,60 +7,47 @@ import {
 } from "vue-cli-plugin-electron-builder/lib";
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win: BrowserWindow | any;
+let mainWindow: BrowserWindow | any;
 
-// Standard scheme must be registered before the app is ready
 protocol.registerStandardSchemes(["app"], { secure: true });
 function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+  });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
-    if (!process.env.IS_TEST) { win.webContents.openDevTools(); }
+    mainWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
+    if (!process.env.IS_TEST) { mainWindow.webContents.openDevTools(); }
   } else {
     createProtocol("app");
-    // Load the index.html when not in development
-    win.loadURL("app://./index.html");
+    mainWindow.loadURL("app://./index.html");
   }
 
-  win.on("closed", () => {
-    win = null;
+  mainWindow.on("closed", () => {
+    mainWindow = null;
   });
 }
 
-// Quit when all windows are closed.
+app.on("ready", async () => {
+  if (isDevelopment && !process.env.IS_TEST) {
+    await installVueDevtools();
+  }
+  createWindow();
+});
+
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+  if (mainWindow === null) {
     createWindow();
   }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    await installVueDevtools();
-  }
-  createWindow();
-});
-
-// Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === "win32") {
     process.on("message", (data) => {
