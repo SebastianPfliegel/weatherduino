5cff70391a561ff2e87373511b4c12a28110e6f2
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2018-12-25 19:11:40 +0100

    Use Vue cli to build electron app

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,41 +1,8 @@
-import {app, BrowserWindow} from "electron";
-import {enableLiveReload} from "electron-compile";
-import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
-import * as path from "path";
+import Vue from 'vue';
+import App from './App.vue';
 
-const isDevMode = process.execPath.match(/[\\/]electron/);
-if (isDevMode) {
-    enableLiveReload();
-}
+Vue.config.productionTip = false;
 
-let mainWindow: Electron.BrowserWindow | null;
-
-async function createWindow() {
-    mainWindow = new BrowserWindow({
-        height: 768,
-        width: 1024,
-    });
-    mainWindow.loadFile(path.join(__dirname, "./index.html"));
-
-    if (isDevMode) {
-        await installExtension(VUEJS_DEVTOOLS);
-    }
-
-    mainWindow.on("closed", () => {
-        mainWindow = null;
-    });
-}
-
-app.on("ready", createWindow);
-
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
-});
-
-app.on("activate", () => {
-    if (window === null) {
-        createWindow();
-    }
-});
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
