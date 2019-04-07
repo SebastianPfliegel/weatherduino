926caa4cb0a2e59e3f2be23300dfc5fde54eb07d
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2019-04-07 19:47:15 +0200

    Enable linting for api

--- api/src/app.js
+++ api/src/app.js
@@ -1,21 +1,21 @@
-import express from "express"
+import express from "express";
 
-const app = express()
+const app = express();
 
 app.use((req, res, next) => {
-    const err = new Error('Not found')
-    err.status = 404
-    next(err)
-})
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+});
 
 app.use((err, req, res, next) => {
-    const status = err.status || 500
+    const status = err.status || 500;
     res
         .status(status)
         .json({
             status,
             message: err.message
-        })
-})
+        });
+});
 
 export default app;
